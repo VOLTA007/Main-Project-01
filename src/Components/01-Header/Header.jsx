5a5f2ee537a3ec0b79a5804985bcb964f478e4d2
Yusuf <volta@gmail.com>
2024-02-19 17:27:14 +0200

    New

--- src/Components/01-Header/Header.jsx
+++ src/Components/01-Header/Header.jsx
@@ -5,17 +5,17 @@ import NavBar from '../02-NavBar/NavBar';
 function Header(){
     return(
         <>
-        <img className={styles.logo} src="./Logo.png"></img>
+        <img className={styles.logo} src="./Logo.png" alt=''></img>
         <NavBar />
         <div className={styles.div2}>
-        <h1 className={styles.hed}>Websites&<br></br>
-        Branding</h1>
-        <button className={styles.button1}>SCROLL DOWN ⇩</button>
+            <h1 className={styles.hed}>Websites&<br></br>
+            Branding</h1>
+            <button className={styles.button1}>SCROLL DOWN ⇩</button>
         </div>
-        <div className={styles.div3}>
-        <p>Let's Talk <br></br>
-        hello@joey.co</p>
-        <p>Hello. I'm Joey. an online product designer focusing<br></br> on brand identity, advertising, and no-code <br></br> instruments.</p>
+            <div className={styles.div3}>
+            <p>Let's Talk <br></br>
+            hello@joey.co</p>
+            <p>Hello. I'm Joey. an online product designer focusing<br></br> on brand identity, advertising, and no-code <br></br> instruments.</p>
         </div>
         </>
     )
