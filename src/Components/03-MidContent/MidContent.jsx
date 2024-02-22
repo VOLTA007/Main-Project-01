1eb5e1c8f1a92cf014dab05b91f15fda5973a8db
Volta <voltagaming2001@gmail.com>
2024-02-22 21:30:23 +0200

    New Update

--- src/Components/03-MidContent/MidContent.jsx
+++ src/Components/03-MidContent/MidContent.jsx
@@ -4,7 +4,28 @@ import styles from './MidContent.module.css';
 function MidContent(){
 
     return(
-        <h1>Wla</h1>
+        <>
+            <div className={styles.div4}>
+                <h1>Projects</h1>
+                <p className={styles.pp}>As a seasoned creator of contemporary.<br></br>
+                    user-friendly web designs and digital<br></br>
+                    solutions, I aim to assist you in constructing<br></br>
+                    the brand of your fantasies.
+                </p>
+            </div>
+            <div className={styles.gridcontainer}>
+                <img className={styles.imggrid} src="./card 1.png" alt="Card 1" />
+                <img className={styles.imggrid} src="./Card 2.png" alt="Card 2" />
+                <h3>Crtly</h3>
+                <h3>Tanasto</h3>
+                <p className={styles.ppp}>Mobile App</p>
+                <p className={styles.ppp}>Web Design clas</p>
+            </div>
+            <div className={styles.gridcontainer2}>
+                <img className={styles.imggrid} src="./Card 3.png" alt="Card 3" />
+                <img className={styles.imggrid} src="./Card 4.png" alt="Card 4" />
+            </div>
+        </>
     );
 }
 
