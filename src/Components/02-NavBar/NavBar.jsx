14c1e48b7ec47907db36c64df04ddba3b3e68332
Yusuf <volta@gmail.com>
2024-03-02 20:03:32 +0200

    New Update

--- src/Components/02-NavBar/NavBar.jsx
+++ src/Components/02-NavBar/NavBar.jsx
@@ -4,6 +4,16 @@ import styles from './NavBar.module.css';
 function NavBar() {
   const [isHovered, setIsHovered] = useState(false);
   const [hoveredItem, setHoveredItem] = useState('');
+  const [lastHoveredPosition, setLastHoveredPosition] = useState(null);
+  const [isFocus, setIsFocus] = useState(null);
+
+  const handleBlur = () => {
+    setIsFocus(null);
+  };
+
+  const handleFocus = (item) => {
+    setIsFocus(item);
+  };
 
   const handleMouseEnter = (item) => {
     setIsHovered(true);
@@ -12,29 +22,55 @@ function NavBar() {
 
   const handleMouseLeave = () => {
     setIsHovered(false);
-    setHoveredItem('');
+    // Store the last hovered item's position
+    setLastHoveredPosition(getPosition());
   };
 
-  let animationStyle = styles.animation;
-
-  if (isHovered && hoveredItem === 'home') {
-    animationStyle = `${styles.animation} ${styles.hovered1}`;
-  } else if (isHovered && hoveredItem === 'projects') {
-    animationStyle = `${styles.animation} ${styles.hovered2}`;
-  } else if (isHovered && hoveredItem === 'about') {
-    animationStyle = `${styles.animation} ${styles.hovered3}`;
-  } else if (isHovered && hoveredItem === 'contact') {
-    animationStyle = `${styles.animation} ${styles.hovered4}`;
+  // Get the position of the last hovered item
+  const getPosition = () => {
+  if (hoveredItem === 'home') {
+    return '2%';
+  } else if (hoveredItem === 'projects') {
+    return '24.7%';
+  } else if (hoveredItem === 'about') {
+    return '53.5%';
+  } else if (hoveredItem === 'contact') {
+    return '76%';
+  } else {
+    return '0%';
   }
+};
+
+  // Determine animation styles based on hoveredItem state
+  const getAnimationStyle = () => {
+    
+     if (isHovered) {
+      if (hoveredItem === 'home') {
+        return `${styles.animation} ${styles.hovered1} `;
+      } else if (hoveredItem === 'projects') {
+        return `${styles.animation} ${styles.hovered2} `;
+      } else if (hoveredItem === 'about') {
+        return `${styles.animation} ${styles.hovered3} `;
+      } else if (hoveredItem === 'contact') {
+        return `${styles.animation} ${styles.hovered4} `;
+      } else {
+        return styles.animation;
+      }
+    } else {
+      return `${styles.animation} ${styles.exitAnimation}`;
+    }
+  };
+
+  const animationStyle = getAnimationStyle();
 
   return (
     <div className={styles.div1}>
-      <nav>
-        <a href='#' onMouseEnter={() => handleMouseEnter('home')} onMouseLeave={handleMouseLeave}>Home</a>
-        <a href='#' onMouseEnter={() => handleMouseEnter('projects')} onMouseLeave={handleMouseLeave}>Projects</a>
-        <a href='#' onMouseEnter={() => handleMouseEnter('about')} onMouseLeave={handleMouseLeave}>About</a>
-        <a href='#' onMouseEnter={() => handleMouseEnter('contact')} onMouseLeave={handleMouseLeave}>Contact</a>
-        <div className={animationStyle} />
+      <nav onBlur={() => handleBlur()}>
+        <a href='#' className={isFocus === 'home' ? styles.focused : null} onFocus={() => handleFocus('home')} onMouseEnter={() => handleMouseEnter('home')} onMouseLeave={handleMouseLeave}>Home</a>
+        <a href='#' className={isFocus === 'projects' ? styles.focused : null} onFocus={() => handleFocus('projects')} onMouseEnter={() => handleMouseEnter('projects')} onMouseLeave={handleMouseLeave}>Projects</a>
+        <a href='#' className={isFocus === 'about' ? styles.focused : null} onFocus={() => handleFocus('about')} onMouseEnter={() => handleMouseEnter('about')} onMouseLeave={handleMouseLeave}>About</a>
+        <a href='#' className={isFocus === 'contact' ? styles.focused : null} onFocus={() => handleFocus('contact')} onMouseEnter={() => handleMouseEnter('contact')} onMouseLeave={handleMouseLeave}>Contact</a>
+        <div className={animationStyle} style={{ left: isHovered ? getPosition() : lastHoveredPosition }} />
       </nav>
     </div>
   );
